fba3fb7dd721ff7fe05a8833e32967ee43673b65
Ramaj57 <slamaster02@yahoo.com>
2025-03-27 14:47:15 -0400

    registered new component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { PreorderButtonComponent } from "./preorder-button/preorder-button.component";
 import { FeaturesCardComponent } from "./features-card/features-card.component";
 import { buttonData, cardData } from '../data';
+import { BlankSpaceComponent } from "./blank-space/blank-space.component";
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [PreorderButtonComponent, FeaturesCardComponent],
+  imports: [PreorderButtonComponent, FeaturesCardComponent, BlankSpaceComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
